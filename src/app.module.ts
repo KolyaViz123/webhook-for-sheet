36dabae02a3258f96041e81255554501644093c4
Kolya Viz <k1vizenko@gmail.com>
2024-11-25 17:01:36 +0200

    generate webhool module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Row } from './models/row.model';
+import { WebhookModule } from './webhook/webhook.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, 
     }),
+    WebhookModule,
     // SequelizeModule.forRoot({
     //   dialect: 'postgres',
     //   host: process.env.DATABASE_HOST, 
